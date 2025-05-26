1ad0fcf3ee688970237158b0cf5fd7bb0bc876a1
vilecoder <147159797+vilecoder@users.noreply.github.com>
2025-05-26 17:13:48 +0000

    studied till data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 2221
+let accountEmail = "vg@xyz.com"
+var accountPassword = "fff" 
+// var has a scope problem so dont use this use only let and a variable may also be defined without a keyword see below
+
+accountCity = "Jaipur"
+
+accountEmail = "dhfihe@nith.com"
+
+let accountState//if defined only but value not assigned so it takes undefined value just like garbage value in c
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])//this is used to print variables in a tabular format
+

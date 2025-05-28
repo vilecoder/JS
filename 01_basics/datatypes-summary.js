34e3d0977d90faee4318ab75f0437be607d22959
vilecoder <147159797+vilecoder@users.noreply.github.com>
2025-05-28 04:55:41 +0000

    new

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,29 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack(for primitive data type) and Heap(for non primitive data type : arr,fn,obj)
+
+let value = 1234
+let anotherValue = value // a copy of variable value given to anotherVAlue
+anotherValue =333
+// console.log(anotherValue);
+// console.log(value);
+
+
+
+
+let userOne = {
+    email : "harshvardhan@xyz",
+    numb : "12345",
+}
+
+let userTwo = userOne;
+userTwo.numb = "22344"
+
+console.log(userOne.numb);
+console.log(userTwo.numb);
